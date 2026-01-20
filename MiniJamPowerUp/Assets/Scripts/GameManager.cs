3b33df3fa3f13a2efacfa019817b3eb5fa543530
Zeya <bzlongid@gmail.com>
2026-01-20 12:20:35 -0800

    weapon mechanics

--- MiniJamPowerUp/Assets/Scripts/GameManager.cs
+++ MiniJamPowerUp/Assets/Scripts/GameManager.cs
@@ -1,18 +1,56 @@
 #nullable enable
 
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*
+ *  Controlls the state of the following: 
+ *      wave (enemy) information, 
+ *      weapon information, 
+ *      mouse position
+ */
+
 public class GameManager : MonoBehaviour
 {
-    public static GameManager? Instance { get; private set; }
+    public static GameManager? Instance { get; private set; } = null!;
+
+    // constants
+    public const int MAX_WAVES = 7;
+
+    // tunables
+    [SerializeField] private int[] enemiesPerWave = new int[MAX_WAVES] { 5, 10, 15, 20, 25, 30, 50 };
+    [SerializeField] private WaitForSeconds enemySpawnRate = new WaitForSeconds(1f);
+    [SerializeField] private List<Transform> enemySpawnPoints = new List<Transform>();
 
     // refrences
     [SerializeField] Camera? mainCamera;
 
+    // ammo prefabs - can break this out into another manager
+    [Serializable]
+    public class AmmoMapping
+    {
+        public AmmoPrefab AmmoType;
+        public GameObject? AmmoPrefab;
+    }
+
+    [SerializeField] private List<AmmoMapping> ammoMappings = new List<AmmoMapping>();
+    private Dictionary<AmmoPrefab, GameObject> ammoDict = new Dictionary<AmmoPrefab, GameObject>();
+
     // global vars
     public Quaternion MousePosition { get; private set; }
-    public WaitForSeconds FireAmmoInterval = new WaitForSeconds(3f);
+
+    public int CurWave { get; private set; } = 1;
+
+    public int EnemiesInWave { get; private set; }
+    public int EnemiesLeft { get; private set; }
+    public int CurEnemySpawnPointIndex { get; private set; }
+
+    public Weapon CurWeapon = Weapon.Single; // { get; private set; } = Weapon.Single;      //FOR DEBUG ONLY RN
+    public GameObject? CurAmmoPrefab { get; private set; }
+    public AmmoSpread CurAmmoSpread { get; private set; }
+    public WaitForSeconds FireAmmoInterval { get; private set; } = new WaitForSeconds(1f);
 
     void Awake()
     {
@@ -23,7 +61,17 @@ public class GameManager : MonoBehaviour
         }
         Instance = this;
 
+        // build ammo prefab refs - can break this out into another manager
+        ammoDict = new Dictionary<AmmoPrefab, GameObject>();
+        foreach (var mapping in ammoMappings)
+        {
+            if(mapping.AmmoPrefab != null)
+                ammoDict[mapping.AmmoType] = mapping.AmmoPrefab;
+        }
+
         UpdateMosePosition();
+        SetWeapon(CurWeapon);
+        // start wave
 
         // Optional: Keep the object alive when loading new scenes
         //DontDestroyOnLoad(this.gameObject);
@@ -32,6 +80,47 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         UpdateMosePosition();
+    }
+
+    public void OnEnemyEliminated()
+    {
+        EnemiesLeft--;
+
+        if (EnemiesLeft <= 0) OnWaveEnded();
+    }
+
+    private void OnWaveEnded()
+    {
+        // put anything to signal the wave end here i.e. sound, cutscene, animations, etc.
+
+        // update the wave info
+        CurWave++;
+        if (CurWave > MAX_WAVES)
+        {
+            OnGameEnded();
+            return;
+        }
+
+        EnemiesInWave = enemiesPerWave[CurWave];
+        EnemiesLeft = EnemiesInWave;
+
+        // update the player weapon
+        var weaponTypes = Enum.GetValues(typeof(Weapon));
+        if (weaponTypes.Length >= CurWave)
+        {
+            SetWeapon((Weapon)weaponTypes.GetValue(CurWave));
+        }
+        else
+        {
+            Debug.LogError($"GameManager - Could not find a weapon for wave {CurWave}! Defaulting to game end...");
+            OnGameEnded();
+            return;
+        }
+    }
+
+    private void OnGameEnded()
+    {
+
     }
 
     private void UpdateMosePosition()
@@ -57,5 +146,14 @@ public class GameManager : MonoBehaviour
                 MousePosition = Quaternion.LookRotation(direction);
             }
         }
+    }
+
+    private void SetWeapon(Weapon weapon)
+    {
+        // update the state of the weapon
+        CurWeapon = weapon;
+        CurAmmoPrefab = ammoDict[CombatVariables.WeaponPrefabMap[weapon]];
+        CurAmmoSpread = CombatVariables.WeaponSpreadMap[weapon];
+        FireAmmoInterval = CombatVariables.WeaponReloadTimeMap[weapon];
     }
 }
